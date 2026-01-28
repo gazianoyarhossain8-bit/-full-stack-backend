f6b027a3e9a4d02b42165cd5715c08150e4e3474
gazianoyarhossain8-bit <gazianoyarhossain8@gmail.com>
2026-01-28 09:06:09 +0530

    server upload

--- server.js
+++ server.js
@@ -1,26 +1,42 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import cors from 'cors';
-import userRouter from './routes/userRouter.js';
-import authRoute from './routes/authRoute.js'
+import cors from "cors";
+
+import userRouter from "./routes/userRouter.js";
+import authRoute from "./routes/authRoute.js";
 import protect from "./middleware/authMiddleware.js";
 
 dotenv.config();
 
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
+let isConnected = false;
 
+const connectDB = async () => {
+  if (isConnected) return;
 
-app.use(cors())
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    isConnected = true;
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log("MongoDB error:", error);
+  }
+};
 
+connectDB();
 
-app.use("/api",protect,userRouter)
-app.use("/auth", authRoute)
+// routes
+app.use("/api", protect, userRouter);
+app.use("/auth", authRoute);
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("mongodb connected"))
-.catch((err) => console.log(err));
+app.get("/", (req, res) => {
+  res.send("Backend running 🚀");
+});
+
 export default app;
