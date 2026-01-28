a9b70076b9a53cdacf172f9da7a5fbea67a01e62
gazianoyarhossain8-bit <gazianoyarhossain8@gmail.com>
2026-01-28 08:28:28 +0530

    redeploy

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import cors from 'cors';
 import userRouter from './routes/userRouter.js';
 import authRoute from './routes/authRoute.js'
 import protect from "./middleware/authMiddleware.js";
+import e from "express";
 
 dotenv.config();
 
@@ -17,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors())
 
 
-app.use("/api",userRouter)
+app.use("/api",protect,userRouter)
 app.use("/auth", authRoute)
 
 
@@ -25,7 +26,5 @@ mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("Mongodb Atlas connected"))
 .catch((err) =>{
   console.log('error',err)
-})
-
-
+});
 export default app;
