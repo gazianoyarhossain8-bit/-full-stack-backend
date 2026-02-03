94649a107b9b343cefcb2dad5dcfb8c94d383e06
gazianoyarhossain8-bit <gazianoyarhossain8@gmail.com>
2026-02-03 08:38:30 +0530

    only cors

--- server.js
+++ server.js
@@ -12,16 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin",
-    "https://full-stack-frontend-virid.vercel.app");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    next();
-});
-app.options("*", (req, res) => {
-  res.sendStatus(200);
-});
+app.use(cors());
                                                         
   
 
