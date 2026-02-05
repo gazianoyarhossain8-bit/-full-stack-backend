1b7cac467d7334d9fe0365af66aecb65d36f4f26
gazianoyarhossain8-bit <gazianoyarhossain8@gmail.com>
2026-02-05 08:30:41 +0530

    cookie true

--- controller/authController.js
+++ controller/authController.js
@@ -56,10 +56,13 @@ const loginUser = async(req, res) => {
         );
 
         res.cookie("token", token, {
-            httpOnly: true,
-            sameSite: "None",
-            maxAge: 7 * 24 * 60 * 60 * 1000,
+          httpOnly: true,
+          secure: true,       
+          sameSite: "none",    
+          maxAge: 7 * 24 * 60 * 60 * 1000,
+          path: "/"
         });
+        
         res.status(200).json({
             token,
             authId: auth._id,
