bd046bc7dded9a9f65046704d7fc8177a0134684
gazianoyarhossain8-bit <gazianoyarhossain8@gmail.com>
2026-02-20 21:47:50 +0530

    render server modified

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import cookieParser from "cookie-parser";
 
 import userRouter from "./routes/userRouter.js";
 import authRouter from "./routes/authRoute.js";
+const PORT = process.env.PORT || 5000;  
 
 
 dotenv.config();
@@ -33,9 +34,6 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI);
     isConnected = true;
     console.log("MongoDB connected");
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
-    });
   } catch (error) {
     console.log("MongoDB error:", error);
   }
@@ -52,3 +50,6 @@ app.get("/", (req, res) => {
   res.send("Backend is running...");
 });
 
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+}); 
